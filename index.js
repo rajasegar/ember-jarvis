ac42a4016d3dff9d790c86cb3746b137b95a523e
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-12-27 11:05:32 +0530

    [CHORE] treeForVendor hook for octokit/rest

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 'use strict';
 
+const path = require('path');
+const funnel = require('broccoli-funnel');
+const merge = require('broccoli-merge-trees');
 
 module.exports = {
   name: require('./package').name,
-  afterInstall() {
-  // Add @octokit/rest to package.json
-  return this.addPackagesToProject([
-  { name: '@octokit/rest' }
-  ]);
+
+  // Taken from ember-split-view-modifier
+  treeForVendor(tree) {
+    let libPath = path.join(path.dirname(require.resolve('@octokit/rest')), '..');
+    let lib = funnel(this.treeGenerator(libPath));
+    return merge([tree, lib]);
+  },
+
+  included() {
+    this._super.included.apply(this, arguments);
   }
+
 };
