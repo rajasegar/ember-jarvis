5eb58b777f936491aa48e101c32ad0f02687f9ec
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-12-27 10:57:40 +0530

    [CHORE] Use afterInstall hook for octokit/rest

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+  afterInstall() {
+  // Add @octokit/rest to package.json
+  return this.addPackagesToProject([
+  { name: '@octokit/rest' }
+  ]);
+  }
 };
