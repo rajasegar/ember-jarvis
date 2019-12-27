df7eb0425a9fb94d2acf0a2f6de1acd9b3d71382
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-12-27 11:42:02 +0530

    [CHORE] Removing codemod service from addon
    1. Removing unnecessary hooks from index.js
    2. Remove octokit/rest dependency

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 'use strict';
 
-const path = require('path');
-const funnel = require('broccoli-funnel');
-const merge = require('broccoli-merge-trees');
-
 module.exports = {
-  name: require('./package').name,
-
-  // Taken from ember-split-view-modifier
-  treeForVendor(tree) {
-    let libPath = path.join(path.dirname(require.resolve('@octokit/rest')), '..');
-    let lib = funnel(this.treeGenerator(libPath));
-    return merge([tree, lib]);
-  },
-
-  included() {
-    this._super.included.apply(this, arguments);
-    this.import('vendor/@octokit/rest/index.js');
-  }
-
+  name: require('./package').name
 };
